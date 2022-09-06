1a430400f20ee149c11481bf0782a3954bc353e5
Chris Li <chris.lty07@gmail.com>
2022-09-05 23:45:16 -0400

    Added Smooth Scrolling to index.js

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// Smooth Scrolling to each element
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => { anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
