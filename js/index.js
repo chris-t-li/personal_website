bc63dd9c5df83264c64e38c801cf884a1f633938
Chris Li <chris.lty07@gmail.com>
2022-09-06 21:59:03 -0400

    Added email JS Code

--- js/index.js
+++ js/index.js
@@ -7,3 +7,16 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => { anchor.addEventLis
         });
     });
 });
+
+// Email 
+
+document.querySelector('section#contact button').addEventListener('click', sendEmail);
+
+function sendEmail() {
+    const link = "mailto:skyy_@live.com"
+                + "?cc=" + encodeURIComponent(document.getElementById('contactEmail').value)
+                + "&subject=" + encodeURIComponent("Hi Chris, my name is " + document.getElementById('contactName').value)
+                + "&body=" + encodeURIComponent(document.querySelector('section#contact form textarea').value)
+                ;
+    window.location.href = link;
+}
